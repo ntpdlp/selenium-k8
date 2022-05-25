6971ec697851dffec3f59baf8e98d147bd103122
ntpdlp <30646772+ntpdlp@users.noreply.github.com>
2022-05-25 05:56:39 -0500

    lab 02

--- src/lab_02/CalculateBMI.java
+++ src/lab_02/CalculateBMI.java
@@ -13,14 +13,20 @@ public class CalculateBMI {
         weight = scanner.nextFloat();
         bmi = bmiIndex(height,weight);
 
+        final float NORMAL_MIN = 18.5f;
+        final float NORMAL_MAX = 24.9f;
+
         if(bmi<=18.5){
             System.out.println("Underweight");
+            System.out.println("Solution: increase weight " + (NORMAL_MIN*height*2 - weight) + "kg");
         } else if(bmi<=24.9) {
             System.out.println("Normal weight");
         }else if(bmi<=29.9){
             System.out.println("Overweight");
+            System.out.println("Solution: decrease weight " + (NORMAL_MAX*height*2 - weight) + "kg");
         }else{
             System.out.println("Obesity");
+            System.out.println("Solution: decrease weight " + (NORMAL_MAX*height*2 - weight) + "kg");
         }
     }
 
